f49b24105a28c13fa0b1745ca1105003f1c93546
Nguyen Quang Trung <trungnq1@itsol.vn>
2026-03-06 13:53:49 +0700

    commit code

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // @ts-ignore
 export const API_URL = import.meta.env.DEV
     ? 'http://localhost:3000/api/convert'
-    : 'https://pdf-to-docx-backend-trungq.vercel.app/api/convert';  // ← Thay Vercel URL sau deploy
+    : 'https://trungq1405-pdf-to-docx-converter-ba-chi.vercel.app/';  // ← Thay Vercel URL sau deploy
